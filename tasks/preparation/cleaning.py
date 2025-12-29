15ce4067772f73365989deaa958a80bc00a06991
rafalposwiata <rafalposwiata@gmail.com>
2025-12-29 13:19:22 +0100

    retrieval datasets preparation + refactoring

--- tasks/preparation/cleaning.py
+++ tasks/preparation/cleaning.py
@@ -6,33 +6,39 @@ from datasets import Dataset, DatasetDict
 def remove_multiple_spaces(text: str) -> str:
     return re.sub(r"\s+", " ", text)
 
+
 # Implementation based on https://gist.github.com/AlexeyVatolin/ea3adc21aa7a767603ff393b22085adc
 class DatasetCleaner:
 
-    def __init__(self, text_field: str, min_words: int):
+    def __init__(self, text_field: str = None, min_words: int = 3):
         self.text_column: str = text_field
         self.min_words: int = min_words
 
-    def clean(self, dataset: Union[Dataset, DatasetDict]) -> tuple[
-        Union[Dataset, DatasetDict], dict[str, dict]]:
+    def clean(self, dataset: DatasetDict, text_column: str = None, validate_leakage: bool = True,
+              skip_splits: List = None) -> tuple[DatasetDict, dict[str, dict]]:
+        if self.text_column is None:
+            self.text_column = text_column
         report: dict[str, dict] = dict()
         for split in dataset.keys():
-            report[split] = {"original_num_of_rows": dataset[split].num_rows}
-            for func_name, func in self.get_cleaning_funcs():
-                before = dataset[split].num_rows
-                dataset[split] = func(dataset[split])
-                after = dataset[split].num_rows
-                report[split]["num_of_rows"] = after
-                report[split][func_name] = before - after
+            if skip_splits is None or split not in skip_splits:
+                report[split] = {"original_num_of_rows": dataset[split].num_rows}
+                for func_name, func in self.get_cleaning_funcs():
+                    before = dataset[split].num_rows
+                    dataset[split] = func(dataset[split])
+                    after = dataset[split].num_rows
+                    report[split]["num_of_rows"] = after
+                    report[split][func_name] = before - after
 
-        for split in dataset.keys():
-            if split == "train":
-                continue
-            before = dataset[split].num_rows
-            dataset[split] = self.filter_leakage(dataset["train"], dataset[split])
-            after = dataset[split].num_rows
-            report[split]["num_of_rows"] = after
-            report[split]["leakage_from_train"] = before - after
+        if validate_leakage:
+            for split in dataset.keys():
+                if skip_splits is None or split not in skip_splits:
+                    if split == "train":
+                        continue
+                    before = dataset[split].num_rows
+                    dataset[split] = self.filter_leakage(dataset["train"], dataset[split])
+                    after = dataset[split].num_rows
+                    report[split]["num_of_rows"] = after
+                    report[split]["leakage_from_train"] = before - after
 
         return dataset, report
 
@@ -42,7 +48,7 @@ class DatasetCleaner:
             ("deduplicate_exact", lambda _dataset: self.deduplicate(_dataset)),
             ("deduplicate_lower", lambda _dataset: self.deduplicate(_dataset, to_lower=True)),
             ("deduplicate_lower_normalized_spaces", lambda _dataset: self.deduplicate(_dataset, to_lower=True,
-                                                                                           normalized_spaces=True)),
+                                                                                      normalized_spaces=True)),
             ("short_texts", self.filter_short_texts),
         ]
 
--- tasks/preparation/mteb_datasets_verification.py
+++ tasks/preparation/mteb_datasets_verification.py
@@ -1,5 +1,5 @@
 import logging
-from tasks.preparation.datasets_preparation import BaseDataset, TaskType
+from tasks.preparation.datasets_preparation import BaseDataset, TaskType, RetrievalDataset
 
 
 class MassiveIntent(BaseDataset):
@@ -16,13 +16,22 @@ class MassiveScenario(BaseDataset):
                          subset="pl", min_words=2)
 
 
+class ArguAna_PL(RetrievalDataset):
+
+    def __init__(self):
+        super().__init__("arguana_pl", "mteb/ArguAna-PL", queries_path="queries", corpus_path="corpus",
+                         split="test", qrels_path="default")
+
+
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s : %(message)s', level=logging.INFO)
     logging.root.setLevel(logging.INFO)
 
     for task in [
         MassiveIntent,
-        MassiveScenario
+        MassiveScenario,
+
+        ArguAna_PL
     ]:
         _task = task()
         logging.info(f"Verifying {_task.name}")
